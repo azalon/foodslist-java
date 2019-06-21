24376ca31c72f0cfd7b05e743bed969792fc5df3
Avalon <avalongalioto@gmail.com>
2019-06-21 09:38:40 -0500

    started project

---
+++ src/main/java/org/launchcode/foodslist/FoodslistApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.foodslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodslistApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodslistApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/launchcode/foodslist/FoodslistApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.foodslist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodslistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
